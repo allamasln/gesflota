574f0e455c9de9bb6689a31f9bfa49d53729fcd1
Antonio Llamas <antoniollamasln@gmail.com>
2019-10-15 16:21:44 +0200

    add build

---
+++ server/public/precache-manifest.52229861224d9415cf73bf62bf4487ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f325db6a4752e289586ee4c757a5ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88477187eea3460aad1d",
+    "url": "/static/css/main.a6057923.chunk.css"
+  },
+  {
+    "revision": "5c8c8cefb27e82c4aaa1",
+    "url": "/static/js/2.ff99732a.chunk.js"
+  },
+  {
+    "revision": "88477187eea3460aad1d",
+    "url": "/static/js/main.d5c0e3ef.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  }
+]);
